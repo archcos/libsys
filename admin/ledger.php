7fa8da4cb4c839c821f97b0f7804f009d86e8d1f
archcos <arjay.charcos25@gmail.com>
2025-06-02 15:43:47 +0800

    Ledger Update

--- admin/ledger.php
+++ admin/ledger.php
@@ -487,6 +487,7 @@ if ($filter_type == 'monthly') {
     }
 }
 </style>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
 <script>
 $(document).ready(function() {
